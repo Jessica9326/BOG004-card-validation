0ac95a58f6087af30b869025f9b9e44002540e60
Jessica Sierra <jessica.sierraramos@gmail.com>
2022-01-17 18:24:46 -0500

    maquetado y funcion is valid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+//Elementos del DOM (documento HTML)
+const inputNombre=document.getElementById("nombre");
+const btnEnviar=document.getElementById("enviar");
+const btnValidar=document.getElementById("validar");
+const mensajeBienvenida=document.getElementById("mensajedebienvenida");
+const inputTarjeta=document.getElementById("tarjeta");
+const mensajeTarjeta=document.getElementById("mensajetarjeta");
+
+//Escuchadores de Eventos
+btnEnviar.addEventListener("click", enviarNombre);
+btnValidar.addEventListener("click", validarTarjeta); 
+
+//Metodos
+function enviarNombre () {
+    if (inputNombre.value == "") {
+        alert("Por favor digite su nombre");
+    }
+    else {
+        mensajeBienvenida.innerHTML = "Bienvenido " + inputNombre.value;
+        mostrarTarjeta()
+    }
+}
+
+function validarTarjeta () {
+    const numeroTarjeta=Array.from(inputTarjeta.value)
+    
+    
+    if (numeroTarjeta== "") {
+        alert("Digite el numero de su tarjeta");
+    }
+    else {
+        mensajeTarjeta.innerHTML = "Validando su tarjeta...";
+        if (validator.isValid(numeroTarjeta)) {
+            mensajeTarjeta.innerHTML = "Su tarjeta es valida";
+        }
+        else {
+            mensajeTarjeta.innerHTML = "Su tarjeta es invalida";
+        }
+    }
+}
+
+
+
+function mostrarTarjeta () {
+    console.log("ok")
+}
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
 const validator = {
-  // ...
-};
+  isValid(numeroTarjeta) {
+    let resultado=false;
+  
+    
+      const invertido=numeroTarjeta.reverse();
+      const procesado=[]
+      console.log(invertido);
+      for (let i = 0; i<numeroTarjeta.length; i++) { 
+        console.log(numeroTarjeta[i])
+        const posicion=i+1
+        if(posicion%2==0) {
+          //console.log("par")
+          let n = numeroTarjeta[i]*2
+          if(n>=10) {
+            const digitos= Array.from(n.toString())
+             n=parseInt(digitos[0]) + parseInt(digitos[1])
+          } 
+          procesado.push(n)
+        }
+        else {
+          //console.log("impar")
+          procesado.push(parseInt(numeroTarjeta[i]))
+        }
+      }
+      let sum= 0;
+      for( let i=0; i<procesado.length; i++) {
+        sum += procesado[i];
+      }
+      console.log(sum%10==0)
+      console.log(sum)
 
+      console.log(procesado)
+    return sum%10==0;
+  }
+};
 export default validator;
